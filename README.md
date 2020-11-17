1a3694cfc1eb6cf2e8b330b18828409b9ab4929e
maitrimer <maitrimer@gmail.com>
2020-11-17 21:21:30 +0530

    Test to verify tender details

---
+++ README.md
@@ -0,0 +1,8 @@
+## General info
+This project includes simple test cases for the assertion of the tender details
+
+## Setup
+To run this project, follow the below steps:
+1. Clone/Download the git repository
+2. Run command 'npm i'
+3. Run command 'npm test' to run the test cases
---
+++ src/constants/consts.js
@@ -0,0 +1,13 @@
+const URL = "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020";
+const OPEN_DATE = "DATA DA PUBLICAÇÃO: 30/10/2020";
+const BIDDING_DATE = "DATA DA LICITAÇÃO: 30/10/2020";
+const OBJECTIVE = "Registro de Preços pelo prazo de 12 meses, para aquisição de grampo galvanizado.";
+const NEW_PAGE_TITLE = "portal6.pbh.gov.br";
+
+module.exports = {
+    URL,
+    OPEN_DATE,
+    BIDDING_DATE,
+    OBJECTIVE,
+    NEW_PAGE_TITLE,
+}
